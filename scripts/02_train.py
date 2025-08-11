ea386260884af408275c569b82857bb61989a6fb
olavo-dalberto <olavodalberto921@gmail.com>
2025-08-11 17:34:59 -0300

    add trainer

--- scripts/02_train.py
+++ scripts/02_train.py
@@ -1,21 +1,26 @@
 import sys
 import os
+import yaml
+import math
+import bitsandbytes as bnb
 from pathlib import Path
 from datasets import load_dataset
+from transformers import TrainingArguments, Trainer
 
 # Adiciona o diretório raiz do projeto ao path (prioriza no início para evitar conflitos com pacotes instalados chamados 'src')
 ROOT_DIR = Path(__file__).resolve().parents[1]
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 from src.modeling import load_model, create_data_collator, tokenize_dataset
+from src.train_loop import ModelTrainer
 
 dataset = load_dataset("text", data_files={
     "train": "../data/processed/train.txt",
     "val":   "../data/processed/val.txt",
     "test":   "../data/processed/test.txt"
 })
+
 tokenizer, model = load_model()
-import bitsandbytes as bnb
 
 def probe_quant(model):
     classes = set()
@@ -28,8 +33,6 @@ def probe_quant(model):
 # após carregar:
 probe_quant(model)
 
-data_colator = create_data_collator(tokenizer)
-
 total = 0
 trainable = 0
 for n, p in model.named_parameters():
@@ -38,7 +41,6 @@ for n, p in model.named_parameters():
     if p.requires_grad:
         trainable += num
 print(f"Total params: {total/1e6:.1f}M | Trainable (LoRA): {trainable/1e6:.2f}M | Trainable rate: {trainable/total*100:.2f}%")
-print(data_colator)
 
 tokenized_dataset = tokenize_dataset(tokenizer, dataset)
 
@@ -46,3 +48,10 @@ example = tokenized_dataset["train"][0]
 print("Keys:", example.keys())
 print("input_ids:", example["input_ids"])
 print("attention_mask:", example["attention_mask"])
+
+trainer = ModelTrainer(model, tokenized_dataset, tokenizer)
+trainer.train()
+metrics = trainer.evaluate()
+
+print("Eval loss:", metrics["eval_loss"])
+print("Perplexity:", math.exp(metrics["eval_loss"]))
